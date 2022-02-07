273776c61e7647c538f1d6d673f0f6e401107e73
kuzGo <gorankuzma467@gmail.com>
2022-02-07 19:37:54 +0000

    Complete chat functionality

--- scripts/chat.js
+++ scripts/chat.js
@@ -6,12 +6,13 @@ var measure_id = config.SECRET_MEASURE_ID;
 
 
 class Chatroom {
-    constructor(room,username){
+    constructor(room, username) {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
-    async addChat(message){
+    async addChat(message) {
         // format a chat object
         const now = new Date();
         const chat = {
@@ -23,12 +24,44 @@ class Chatroom {
         // save the chat document
         const response = await this.chats.add(chat);
         return response;
-
+    }
+    getChats(callback) {
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        //update the ui
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
+    updateName(username) {
+        this.username = username;
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub) {
+            this.unsub();
+        }
     }
 }
 
-const chatroom = new Chatroom ('gaming', 'shaun');
+const chatroom = new Chatroom('general', 'shaun');
 
-chatroom.addChat('Hello Everyone')
-.then(() => console.log('chat added'))
-.catch(error=> console.log(error))
+chatroom.getChats((data) => {
+
+    console.log(data);
+});
+
+setTimeout(() => {
+    chatroom.updateRoom('gaming');
+    chatroom.updateName('yoshi')
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat('hello');
+}, 3000);
